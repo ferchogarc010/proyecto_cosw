0daa14d019147967765fb43f378da8037136211e
fercho <fercho@MacBook-Pro-de-fernando.local>
2015-02-15 13:49:25 -0500

    proyecto

---
+++ hibernate-compositionmapping-exercise-template/src/main/java/edu/eci/cosw/persistenceexercises/simplepersistencelayer/SimplePersistenceFacade.java
@@ -0,0 +1,22 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package edu.eci.cosw.persistenceexercises.simplepersistencelayer;
+
+
+import java.sql.Date;
+import org.hibernate.Session;
+
+/**
+ *
+ * @author hcadavid
+ */
+public class SimplePersistenceFacade {
+
+    
+    //implementar las operaciones indicadas en cada punto
+    
+    
+}
