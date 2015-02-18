71cfc9a76cb008e1060bd8d1d78cc942a6c764bf
FabianAlvarez07 <fandresalvarez@hotmail.com>
2015-02-17 19:37:13 -0500

    Test2 identificar Plazoleta mediante coordenadas

--- EFF/src/test/java/EFF/test/Test2.java
+++ EFF/src/test/java/EFF/test/Test2.java
@@ -5,10 +5,97 @@
  */
 package EFF.test;
 
+
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.PlazoletaComida;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.PlazoletaComidaId;
+import java.util.List;
+import org.hibernate.Query;
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.Transaction;
+import org.hibernate.cfg.Configuration;
+import org.hibernate.service.ServiceRegistry;
+import org.hibernate.service.ServiceRegistryBuilder;
+import org.junit.After;
+import org.junit.Assert;
+import org.junit.Before;
+import org.junit.Test;
+
 /**
  *
  * @author FabianAndres
  */
 public class Test2 {
+    private SessionFactory sessionFactory;
+    private Session session = null;
+    
+    /**
+     * Operaciones que se realizan antes de ejecutar el banco de pruebas.
+     * En este caso se crea una misma sesión que será usada en todas las
+     * pruebas.
+     */
+    @Before
+    public void setupSession() {
+        
+        Configuration configuration = new Configuration();
+        configuration.configure("hibernate-inmemory.cfg.xml");
+        ServiceRegistry serviceRegistry = new ServiceRegistryBuilder().applySettings(
+        configuration.getProperties()).buildServiceRegistry();
+        sessionFactory = configuration.buildSessionFactory(serviceRegistry);
+        session=sessionFactory.openSession();
+        
+    }
+        
+    /**
+     * Operaciones que se realizan cuando finalice la ejecución de las pruebas.
+     * En este caso se cierra la sesi_n y la f_brica de sesiones.
+     */
+    @After
+    public void closeResources(){
+        session.close();
+        sessionFactory.close();
+    }
+ 
+    /**
+     * El objetivo de esta prueba es que se puede identificar el centro comercial
+     * a partir de coordenadas geográficas (grados, minutos, segundos).
+     * 
+     */
+    @Test
+    public void segundoTest(){
+        Transaction tx=session.beginTransaction();
+        PlazoletaComida plazoleta = new PlazoletaComida(new PlazoletaComidaId("C.C. Santa fe", "Bogotá"), 1, 1, 1, 'W', 1, 1, 1, 'N');
+        session.save(plazoleta);
+        
+        PlazoletaComida plazoletaAux = identificarPlazoleta(session, 1, 1, 1, 'W', 1, 1, 1, 'N');
+        
+        Assert.assertEquals(plazoleta, plazoletaAux);
+        
+        tx.commit();
+    }
+    
+    public PlazoletaComida identificarPlazoleta(Session s, float gradosLon, float minutosLon, 
+            float segundosLon, char orientacionLon, float gradosLat, 
+            float minutosLat, float segundosLat, char orientacionLat){
+        
+        Query q = s.createQuery("from PlazoletaComida where gradosLon=:gradosLon AND "
+                + "minutosLon=:minutosLon AND segundosLon=:segundosLon AND orientacionLon=:orienteacionLon AND "
+                + "gradosLat=:gradosLat AND minutosLat=:minutosLat AND segundosLat=:segundosLat AND orientacionLat=:orienteacionLat");
+        q.setFloat("gradosLon", gradosLon);
+        q.setFloat("minutosLon", minutosLon);
+        q.setFloat("segundosLon", segundosLon);
+        q.setCharacter("orienteacionLon", orientacionLon);
+        
+        q.setFloat("gradosLat", gradosLat);
+        q.setFloat("minutosLat", minutosLat);
+        q.setFloat("segundosLat", segundosLat);
+        q.setCharacter("orienteacionLat", orientacionLat);
+        List<PlazoletaComida> list = q.list();
+        
+        return list.get(0);
+    
+    }
+    
+    
     
 }
