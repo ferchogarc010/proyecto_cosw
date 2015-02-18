31466b30977d6ba7a4b7ea14aec57f9f6d51e306
jennibarajas <jennibarajas@192.168.0.6>
2015-02-17 20:50:26 -0500

    Test 3

--- EFF/src/test/java/EFF/test/Test3y5.java
+++ EFF/src/test/java/EFF/test/Test3y5.java
@@ -5,8 +5,20 @@
  */
 package EFF.test;
 
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Categoria;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Cliente;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Franquicia;
 import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Pago;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Pedido;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.PedidoProducto;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.PlazoletaComida;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.PlazoletaComidaId;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Producto;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.ProductoId;
+import edu.eci.cosw.proyecto_eff.simplepersistencelayer.Sucursal;
 import java.sql.Date;
+import java.util.Calendar;
+import org.hibernate.Query;
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.Transaction;
@@ -58,9 +70,131 @@ public class Test3y5 {
     public void sampleTest(){
         Transaction tx=session.beginTransaction();
         
-        //Cálculo del valor total pagado a una franquicia en un intervalo de fechas.
-        Date d= new Date(date);
-            Pago p= new Pago(5, new Date , monto, null)
+        // Creación de datos para realizar la consulta
+        PlazoletaComida plazoleta = new PlazoletaComida(new PlazoletaComidaId("C.C. Santa fe", "Bogotá"), 1, 1, 1, 'W', 1, 1, 1, 'N');
+        session.save(plazoleta);
+        
+        Franquicia franquicia = new  Franquicia("Burguer King", new Float(12.5));
+        session.save(franquicia);
+        Sucursal sucursal = new Sucursal(franquicia, plazoleta, "12345");
+        session.save(sucursal);
+        Categoria categoria = new Categoria("hamburguesa");
+        session.save(categoria);
+        Categoria categoria2 = new Categoria("Combo");
+        session.save(categoria2);
+        Categoria categoria3 = new Categoria("helado");
+        session.save(categoria3);
+        Producto producto = new Producto(new ProductoId("45", sucursal.getIdSucursales()), categoria, sucursal, 12500, true, "hamburguesa doble carne y papas", 0);
+        session.save(producto);
+        Producto producto2 = new Producto(new ProductoId("46", sucursal.getIdSucursales()), categoria2, sucursal, 10000, true, "combo de 10000", 0);
+        session.save(producto2);
+        Producto producto3 = new Producto(new ProductoId("47", sucursal.getIdSucursales()), categoria3, sucursal, 8000, true, "pastel helado", 0);
+        session.save(producto3);
+        
+        Franquicia franquicia2 = new  Franquicia("Hamburguesa el corral", new Float(15));
+        session.save(franquicia2);
+        Sucursal sucursal2 = new Sucursal(franquicia2, plazoleta, "12345");
+        session.save(sucursal2);
+        Producto p1 = new Producto(new ProductoId("C45", sucursal2.getIdSucursales()), categoria, sucursal2, 12000, false, "Corralisima", new Float(0.10));
+        session.save(p1);
+        Producto p2 = new Producto(new ProductoId("C46", sucursal2.getIdSucursales()), categoria, sucursal2, 15000, false, "Corral doble carne", 0);
+        session.save(p2);
+        Producto p3 = new Producto(new ProductoId("C47", sucursal2.getIdSucursales()), categoria, sucursal2, 15000, false, "todoterreno", new Float(0.5));
+        session.save(p3);
+        
+         //Genera Cliente 
+        
+        Cliente cliente = new Cliente("olivajx@hotmail.com", "sora", "Jennifer", "Barajas", "3102303936");
+        session.save(cliente);
+        Cliente cliente2 = new Cliente("mariaOliveros@gmail.com", "1235", "Maria Teresa", "Oliveros", "3118756238");
+        session.save(cliente);
+        Cliente cliente3 = new Cliente("Euclides@yahoo.com", "orion12", "Euclides", "Rey", "3152362993");
+        session.save(cliente);
+        
+        
+       //Genera Pedidos
+        
+        Pedido pedido = new Pedido(cliente3, false, false, "en espera");
+        session.save(pedido);
+        Pedido pedido2 = new Pedido(cliente, false, false, "en espera");
+        session.save(pedido2);
+        Pedido pedido3= new Pedido(cliente2, false, false, "en espera");
+        session.save(pedido3);
+        Pedido pedido4 = new Pedido(cliente3, false, false, "en espera");
+        session.save(pedido4);
+        Pedido pedido5 = new Pedido(cliente2, false, false, "en espera");
+        session.save(pedido5);
+        Pedido pedido6 = new Pedido(cliente, false, false, "en espera");
+        session.save(pedido6);
+        
+        
+        PedidoProducto pedidoProducto = new PedidoProducto(pedido, producto);
+        session.save(pedidoProducto);
+        PedidoProducto pedidoProducto2 = new PedidoProducto(pedido, producto2);
+        session.save(pedidoProducto2);
+        
+        PedidoProducto pedidoProducto3 = new PedidoProducto(pedido2, p2);
+        session.save(pedidoProducto3);
+        PedidoProducto pedidoProducto4 = new PedidoProducto(pedido2, p3);
+        session.save(pedidoProducto4);
+        
+        PedidoProducto pedidoProducto5 = new PedidoProducto(pedido3, producto3);
+        session.save(pedidoProducto5);
+        PedidoProducto pedidoProducto6 = new PedidoProducto(pedido3, p1);
+        session.save(pedidoProducto6);
+        
+        PedidoProducto pedidoProducto7 = new PedidoProducto(pedido4, producto2);
+        session.save(pedidoProducto7);
+        PedidoProducto pedidoProducto8 = new PedidoProducto(pedido4, p2);
+        session.save(pedidoProducto8);
+        
+        PedidoProducto pedidoProducto9= new PedidoProducto(pedido5, producto);
+        session.save(pedidoProducto9);
+        PedidoProducto pedidoProducto10 = new PedidoProducto(pedido5, producto);
+        session.save(pedidoProducto10);
+        
+        PedidoProducto pedidoProducto11= new PedidoProducto(pedido6, p3);
+        session.save(pedidoProducto11);
+        PedidoProducto pedidoProducto12= new PedidoProducto(pedido6, p3);
+        session.save(pedidoProducto12);
+        
+       
+        //Generate a date 
+        Calendar cal1 = Calendar.getInstance();
+        cal1.set(2013, Calendar.JANUARY, 9, 7, 11, 11); 
+        
+        Calendar cal2 = Calendar.getInstance();
+        cal2.set(2013, Calendar.JANUARY, 11, 3, 06, 11);
+        
+        Calendar cal3 = Calendar.getInstance();
+        cal3.set(2013, Calendar.JANUARY, 10, 11, 10, 12);
+        
+        Calendar cal4 = Calendar.getInstance();
+        cal4.set(2013, Calendar.JANUARY, 10, 9, 12, 55);
+        
+        Calendar cal5 = Calendar.getInstance();
+        cal5.set(2013, Calendar.JANUARY, 11, 8, 30, 12); 
+        
+        Calendar cal6 = Calendar.getInstance();
+        cal6.set(2013, Calendar.JANUARY, 9, 12, 00, 23); 
+        
+        //Registro de Pagos 
+        
+        Pago pago= new Pago(pedido, new Date (cal1.getTimeInMillis()), 22500, "Visa");
+        session.save(pago);
+        Pago pago2 = new Pago(pedido2, new Date(cal2.getTimeInMillis()),27000 , "Mastercard");
+        session.save(pago2);
+        Pago pago3= new Pago(pedido3, new Date (cal3.getTimeInMillis()), 20000, "Debito Maestro");
+        session.save(pago3);
+        Pago pago4= new Pago(pedido4, new Date (cal4.getTimeInMillis()), 25000, "PayPal");
+        session.save(pago4);
+        Pago pago5= new Pago(pedido5, new Date (cal5.getTimeInMillis()), 25000, "Visa");
+        session.save(pago5);
+        Pago pago6= new Pago(pedido6, new Date (cal6.getTimeInMillis()), 30000, "DinersClub");
+        session.save(pago6);
+       
+         //Cálculo del valor total pagado a una franquicia en un intervalo de fechas.
+        Query q= session.createQuery("");
        
         //comparar el resultado esperado contra el obtenido con un assert
         assertTrue(true==true);
